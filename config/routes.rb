83b285caaf4d003c1475c75c2bdbd763753d9260
Ahmed <sport996@github.com>
2019-07-25 09:35:01 +0300

    PM v4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   resources :homes
-  root 'projects#index'
+  # root 'projects#index'
   devise_for :users
   resources :users
   resources :tasks
